828e8db2e567582bedceda49f01c2273a3159578
PRASHANT <gs2110059@sgsitsindore.in>
2022-11-15 23:33:28 +0530

    some minimal changes

--- finalll.py
+++ finalll.py
@@ -1,9 +1,12 @@
 # CODE FOR VACCINE REGISTRATION AND CERTIFICATE
-
-from datetime import datetime, timedelta   # import date time library to get current time and date
-from geopy.geocoders import Nominatim      # this library is used to get current location
-import random                              # this library is used to get random number
-import cProfile                            # this library is used for profiling
+# import date time library to get current time and date
+from datetime import datetime, timedelta   
+# this library is used to get current location
+from geopy.geocoders import Nominatim    
+# this library is used to get random number  
+import random   
+# this library is used for profiling                           
+import cProfile                            
 loc = Nominatim(user_agent="GetLoc")
 def main():
     global First_Name , Last_Name , location_for_appointment,Aadhar_no,first_dose_date,dob,age,vaccine
