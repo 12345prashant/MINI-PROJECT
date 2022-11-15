4c9238dbf478363bc587b8779501545984acc91e
PRASHANT <gs2110059@sgsitsindore.in>
2022-11-15 18:51:13 +0530

    importing all libraries

---
+++ demo2.py
@@ -0,0 +1,6 @@
+# CODE FOR VACCINE REGISTRATION AND CERTIFICATE
+
+from datetime import datetime, timedelta   # import date time library to get current time and date
+from geopy.geocoders import Nominatim      # this library is used to get current location
+import random                              # this library is used to get random number
+import cProfile                            # this library is used for profiling
